83bd7266e52a0ec1c46f62d31f45a7187f223fa4
smondal8 <54061876+smondal8@users.noreply.github.com>
2023-07-26 19:34:17 +0530

    flyway addition.actuator and skeleton

--- src/main/java/org/soumyadev/crowdfundinnovativeworld/CrowdfundInnovativeWorldApplication.java
+++ src/main/java/org/soumyadev/crowdfundinnovativeworld/CrowdfundInnovativeWorldApplication.java
@@ -1,8 +1,12 @@
 package org.soumyadev.crowdfundinnovativeworld;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Failsafe Logbook API", description = "Description of all the Failsafelogbook API endpoints",contact = @Contact(url = "", name = "crowd fund devTeam", email = "s.mondal8@gmail.com")))
 @SpringBootApplication
 public class CrowdfundInnovativeWorldApplication {
 
