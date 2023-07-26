0cd8de346ebc46b9139b64512ab81ccfa280f024
smondal8 <54061876+smondal8@users.noreply.github.com>
2023-07-26 18:09:53 +0530

    First commit

---
+++ src/main/java/org/soumyadev/crowdfundinnovativeworld/CrowdfundInnovativeWorldApplication.java
@@ -0,0 +1,13 @@
+package org.soumyadev.crowdfundinnovativeworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdfundInnovativeWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdfundInnovativeWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/soumyadev/crowdfundinnovativeworld/CrowdfundInnovativeWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.soumyadev.crowdfundinnovativeworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdfundInnovativeWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
