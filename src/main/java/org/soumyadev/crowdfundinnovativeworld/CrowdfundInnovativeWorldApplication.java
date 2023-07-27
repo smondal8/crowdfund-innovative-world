687d6399cadc8fd5e373759c89141bcb01236f1c
smondal8 <54061876+smondal8@users.noreply.github.com>
2023-07-27 18:18:07 +0530

    authentication & jwt authorization

--- src/main/java/org/soumyadev/crowdfundinnovativeworld/CrowdfundInnovativeWorldApplication.java
+++ src/main/java/org/soumyadev/crowdfundinnovativeworld/CrowdfundInnovativeWorldApplication.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info = @Info(title = "Failsafe Logbook API", description = "Description of all the Failsafelogbook API endpoints",contact = @Contact(url = "", name = "crowd fund devTeam", email = "s.mondal8@gmail.com")))
+@OpenAPIDefinition(info = @Info(title = "CrowdFundWorld API", description = "Description of all the CrowdFundWorld API endpoints",contact = @Contact(url = "", name = "crowd fund devTeam", email = "s.mondal8@gmail.com")))
 @SpringBootApplication
 public class CrowdfundInnovativeWorldApplication {
 
